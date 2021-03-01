5fe7163b9e0da3a51df4aa15ba8ec555e0b5ec78
cmmgw <cecily.martin@gmail.com>
2021-02-28 22:51:08 -0500

    adding js folder

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("static/js/samples.json").then((data) => {
+  d3.json("../static/js/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("static/js/samples.json").then((data) => {
+  d3.json("../static/js/samples.json").then((data) => {
     var metadata = data.metadata;
     console.log(metadata)
 
@@ -65,7 +65,7 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("static/js/samples.json").then((data) => {
+  d3.json("../static/js/samples.json").then((data) => {
 
     // 3. Create a variable that holds the samples array. 
     var samples = data.samples;
@@ -102,7 +102,6 @@ function buildCharts(sample) {
       type: "bar",
       orientation: "h",
       text: otu_labels.slice(0,10).reverse(),
-      colorscale: "Rainbow",
     }];
 
     // 9. Create the layout for the bar chart. 
